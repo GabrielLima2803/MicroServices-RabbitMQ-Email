7d305838d726cb0794eee081fd9e76a0532b9d1e
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-20 23:29:53 -0300

    Create EmailDTO

---
+++ src/main/java/com/email/msemail/dto/EmailDTO.java
@@ -0,0 +1,32 @@
+package com.email.msemail.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class EmailDTO {
+
+ 
+    @NotBlank
+    private String ownerRef;
+    @NotBlank
+    @Email
+    private String emailFrom;
+    @NotBlank
+    @Email
+    private String emailTo;
+    @NotBlank
+    private String subject;
+    @NotBlank
+    private String text;
+
+}
