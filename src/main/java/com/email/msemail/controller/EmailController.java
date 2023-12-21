1fc2312c8b7db1529599eb8de7a4bad8df97bd66
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-21 00:51:19 -0300

    Add method getAll and findById

--- src/main/java/com/email/msemail/controller/EmailController.java
+++ src/main/java/com/email/msemail/controller/EmailController.java
@@ -1,13 +1,23 @@
 package com.email.msemail.controller;
 
 
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 import com.email.msemail.dto.EmailDTO;
 import com.email.msemail.model.Email;
@@ -29,6 +39,21 @@ public class EmailController {
         BeanUtils.copyProperties(emailDto, emailModel);
         emailService.sendEmail(emailModel);
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
+        @GetMapping("/emails")
+    public ResponseEntity<Page<Email>> getAllEmails(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/emails/{id}")
+    public ResponseEntity<Object> getOneEmail(@PathVariable(value="id") Long id){
+        Optional<Email> emailModelOptional = emailService.findById(id);
+        if(!emailModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found.");
+        }else {
+            return ResponseEntity.status(HttpStatus.OK).body(emailModelOptional.get());
+        }
     }
 
 }
