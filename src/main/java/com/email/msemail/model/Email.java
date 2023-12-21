97c58af025c6c399136ff8fda6ef5d5e64bf4678
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-20 23:30:06 -0300

    Create Model Email

---
+++ src/main/java/com/email/msemail/model/Email.java
@@ -0,0 +1,40 @@
+package com.email.msemail.model;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import com.email.msemail.enums.StatusEmail;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Email")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Email implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+
+    
+
+}
