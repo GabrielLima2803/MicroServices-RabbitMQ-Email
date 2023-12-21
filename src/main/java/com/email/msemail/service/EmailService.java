a07ee91b3da16146bc620dafe84520cdf068e1f1
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-20 23:30:21 -0300

    Create Email Service

---
+++ src/main/java/com/email/msemail/service/EmailService.java
@@ -0,0 +1,41 @@
+package com.email.msemail.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.email.msemail.enums.StatusEmail;
+import com.email.msemail.model.Email;
+import com.email.msemail.repositories.EmailRepository;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public Email sendEmail(Email emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+             return emailRepository.save(emailModel);
+        }
+    }
+}
